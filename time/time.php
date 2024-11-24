9239737e02bf6d81a7102f44e50d511655b28e7e
Pojoke <pavlykdmutro@gmail.com>
2024-11-24 16:13:31 +0200

    dz-timer

--- time/time.php
+++ time/time.php
@@ -1,1 +1,11 @@
 <?php
+$today = mktime(0, 0, 0, date("m"), date("d"), date("Y"));
+$tomorrow = mktime(0, 0, 0, date("m"), date("d")+1 , date("Y"));
+echo"Акція розпочалась:" . date("M-d-Y", $today) ; 
+echo"Акція завершиться:" . date("M-d-Y", $tomorrow) ; 
+if($today ==$tomorrow)  {
+    echo "Акція закінчилася";
+}
+if($today !==$tomorrow)  {
+    echo "Акція триває";
+}

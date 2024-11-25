1aac4a995e2e3ace622df3147f9a5ed338ad6947
Pojoke <pavlykdmutro@gmail.com>
2024-11-25 12:45:05 +0200

    dz

--- time/time.php
+++ time/time.php
@@ -1,11 +1,20 @@
 <?php
-$today = mktime(0, 0, 0, date("m"), date("d"), date("Y"));
-$tomorrow = mktime(0, 0, 0, date("m"), date("d")+1 , date("Y"));
-echo"Акція розпочалась:" . date("M-d-Y", $today) ; 
-echo"<br>Акція завершиться:" . date("M-d-Y", $tomorrow) ; 
-if($today ==$tomorrow)  {
-    echo "<br>Акція закінчилася";
+
+date_default_timezone_set('Europe/Kyiv');
+$startDate = strtotime('2023-12-31 20:00:00');
+$endDate = strtotime('2024-12-31 20:00:00');
+
+$timeRemaining = $endDate - $startDate;
+echo "Акція розпочалась 2023-12-31 20:00:00";
+if ($timeRemaining <= 0) {
+    echo "<h2>Акція закінчилась!</h2>";
+} else {
+    $days = floor($timeRemaining / (60 * 60 * 24));
+    $hours = floor(($timeRemaining % (60 * 60 * 24)) / (60 * 60));
+    $minutes = floor(($timeRemaining % (60 * 60)) / 60);
+    $seconds = $timeRemaining % 60;
+
+    
+    echo "<p> Акція закінчиться: <span id='timer'>$days днів, $hours годин, $minutes хвилин, $seconds  секунд </span>.</p>";
 }
-if($today !==$tomorrow)  {
-    echo "<br>Акція триває"; 
-}
+?>
